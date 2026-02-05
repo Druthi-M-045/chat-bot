65339772d0a5df6ced1c884ab70a5bf40b5485ed
Druthi-M-045 <druthimsgowda@gmail.com>
2026-02-05 15:52:32 +0530

    Postman

--- app.py
+++ app.py
@@ -1,39 +1,10 @@
-import os
-from azure.ai.inference import ChatCompletionsClient
-from azure.ai.inference.models import SystemMessage, UserMessage, AssistantMessage
-from azure.core.credentials import AzureKeyCredential
+from flask import Flask
 
-endpoint = "https://models.github.ai/inference"
-model = "gpt-4o-mini"
-token = os.environ["GITHUB_TOKEN"]
+app = Flask(__name__)
 
-client = ChatCompletionsClient(
-    endpoint=endpoint,
-    credential=AzureKeyCredential(token),
-)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-
-messages = [
-    SystemMessage("You need to reply in kannada"),
-]
-
-print("Chatbot started. Type 'exit' or 'quit' to stop.")
-
-while True:
-    user_input = input("You: ")
-    if user_input.lower() in ["exit", "quit"]:
-        break
-
-    messages.append(UserMessage(user_input))
-
-    response = client.complete(
-        messages=messages,
-        temperature=1.0,
-        top_p=1.0,
-        max_tokens=1000,
-        model=model
-    )
-
-    assistant_response = response.choices[0].message.content
-    print("Bot:", assistant_response)
-    messages.append(AssistantMessage(assistant_response))
+if __name__ == "__main__":
+    app.run(debug=True)
